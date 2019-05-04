d10c96c4d9e52471242f8bd9a65c310b43e51044
aminos <amri.amine@protonmail.com>
2019-05-04 23:44:26 +0100

    solving problem 6

---
+++ functions/test6.clj
@@ -0,0 +1,8 @@
+(defn make-thingy [x]
+  (fn [& vars] (identity x)))
+
+(let [n (rand-int Integer/MAX_VALUE)
+      f (make-thingy n)]
+  (assert (= n (f)))
+  (assert (= n (f 123)))
+  (assert (= n (apply f 123 (range)))))
