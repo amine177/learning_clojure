3a4b6ad3ff898b5ec871973b0a3eff73c375ec6a
aminos <amri.amine@protonmail.com>
2019-05-13 13:53:48 +0100

    implementing a sorted map using (sorted-map)

---
+++ sorted-map.clj
@@ -0,0 +1,8 @@
+(def sm (sorted-map
+                    "A" 1
+                    "C" 2
+                    "D" 3
+                    "Z" 4
+                    "T" 5))
+(println "keys:" (keys sm))
+(println "vals:" (vals sm))
