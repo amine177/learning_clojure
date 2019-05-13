0d18062d971cc644f7dead13174fee1baa7d81e1
aminos <amri.amine@protonmail.com>
2019-05-13 20:29:19 +0100

    using :else as a truthy value in order to execute if no condition before it is truthy in a (cond) expression

--- flow_control/cond.clj
+++ flow_control/cond.clj
@@ -3,3 +3,10 @@
   (cond
         (< x 2) "x is less than 2"
         (< x 10) "x is less than 10")))
+
+(let [x 15]
+  (println
+  (cond
+        (< x 2) "x is less than 2"
+        (< x 10) "x is less than 10"
+        :else "x is greater or equal to 10")))
